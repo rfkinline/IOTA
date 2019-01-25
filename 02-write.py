4354e3b3287bc0d8695e7fb532486724453cb245
rfkinline <rfkinline@users.noreply.github.com>
2019-01-25 10:52:40 -0500

    Update 02-write.py

--- 02-write.py
+++ 02-write.py
@@ -5,9 +5,11 @@ import sys
 sys.path.append('/home/pi/MFRC522-python')
 import SimpleMFRC522
 
+reader = SimpleMFRC522.SimpleMFRC522()
+
 try:
     while True:
-        text = input('Your Name: ')
+        text = raw_input('Your Name: ')
         print("Now place tag next to scanner to write")
         id, text = reader.write(text) 
         print("recorded")
