3ec927867056a21015ea53cd0ff0a5059eb852b0
rfkinline <rfkinline@users.noreply.github.com>
2019-01-25 11:29:12 -0500

    Update 01-read.py

--- 01-read.py
+++ 01-read.py
@@ -10,11 +10,9 @@ reader = SimpleMFRC522.SimpleMFRC522()
 print("Hold a tag near the reader")
 
 try:
-    while True:
-        id, text = reader.read()
-        print(id)
-        print(text)
+    id, text = reader.read()
+    print(id)
+    print(text)
 
 finally:
-    print("cleaning up")
 GPIO.cleanup()
